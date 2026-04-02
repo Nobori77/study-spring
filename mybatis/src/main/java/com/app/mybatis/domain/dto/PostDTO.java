a03827cbfb940debcd80d7f92e069457fb13faff
Nobori77 <minginew77@gmail.com>
2026-04-02 13:48:01 +0900

    feat day02

---
+++ mybatis/src/main/java/com/app/mybatis/domain/dto/PostDTO.java
@@ -0,0 +1,17 @@
+package com.app.mybatis.domain.dto;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+public class PostDTO {
+    private Long id;
+    private String postTitle;
+    private String postContent;
+    private Long postReadCount;
+    private Long memberId;
+    private String memberName;
+    private String memberPassword;
+    private String memberEmail;
+}
